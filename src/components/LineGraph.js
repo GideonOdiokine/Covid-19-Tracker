9729526c7a132cff9b27b3fe0339b37e47a03f2a
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-08-10 01:01:47 +0100

    ..

---
+++ src/components/LineGraph.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+
+const LineGraph = () => {
+    const [data, setData] = useState({});
+
+    const buildChartData = (data, casesType = "cases") => {
+        const chartData = [];
+        let lastDataPoint;
+
+        data[casesType].forEach((date) => {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date,
+                    y: data[casesType][date] - lastDatePoint,
+                };
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data[casesType][date];
+        });
+        return chartData;
+    };
+
+    useEffect(() => {
+        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                const chartData = buildChartData(data);
+                setData(chartData);
+            });
+    });
+
+    return (
+        <div>
+            <Line data />
+        </div>
+    );
+};
+
+export default LineGraph;
