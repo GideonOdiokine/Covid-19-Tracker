49ec0bea54fe04878ae2f8f66cedbca04499cf69
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-08-28 01:42:31 +0100

    added config for line

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 
+import numeral from "numeral";
+
 const options = {
     legend: {
         display: false,
@@ -8,18 +10,43 @@ const options = {
     elements: {
         point: {
             radius: 0,
-        }
+        },
     },
     maintainAspectRatio: false,
     tooltips: {
         mode: "index",
         intersect: false,
-        callbacks: function (tooltipItem, data) {
-            return numeral(tooltipItem.value).format("+0,0")
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0");
+            },
         },
     },
-}
-
+    scales: {
+        xAxes: [
+            {
+                type: "time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat: "ll",
+                },
+            },
+        ],
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function (value, index, values) {
+                        return numeral(value).format("0a");
+                    },
+                },
+            },
+        ],
+    },
+};
 const LineGraph = () => {
     const [data, setData] = useState({});
 
