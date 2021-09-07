86c1c8d04ac9476d28f7980473318a5b1a27732a
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-09-07 02:12:26 +0100

    ..

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,12 +4,19 @@ import React from 'react'
 export const Table = ({ countries }) => {
     return (
         <div className="table">
-            {countries.map(({ country, cases }) => (
-                <tr key={cases}>
-                    <td>{country}</td>
-                    <td>{cases}</td>
-                </tr>
-            ))}
+            <table >
+                {countries.map(({ country, cases }) => (
+
+                    <tbody key={country}>
+                        <tr  >
+                            <td>{country}</td>
+                            <td>{cases}</td>
+                        </tr>
+                    </tbody>
+
+                ))
+                }
+            </table >
         </div>
     )
 }
