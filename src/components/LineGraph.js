18cb98def69c915a40b9886de1eb441e83083544
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-09-07 02:17:45 +0100

    change my para

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -71,7 +71,7 @@ const LineGraph = () => {
             await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
                 .then((res) => res.json())
                 .then((data) => {
-                    const chartData = buildChartData(data, "cases");
+                    const chartData = buildChartData(data, "deaths");
                     setData(chartData);
                 });
         };
