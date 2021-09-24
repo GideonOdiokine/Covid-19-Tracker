9df84728c44ca0cac98667ac171baab6ed2ecf21
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-09-24 23:12:04 +0100

    added FormControl to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { MenuItem, FormControl, Select } from '@material-ui/core'
 
 const App = () => {
   return (
-    <div>
-      <h2>Hello React</h2>
-    </div >
+    <div className="app">
+      <h1>COVID_19 TRACKER</h1>
+      <FormControl>
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option one</MenuItem>
+          <MenuItem value="worldwide">Option two</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
   )
 }
 
-export default App
+export default App;
