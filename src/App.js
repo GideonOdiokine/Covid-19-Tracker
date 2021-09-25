2af0a5add31d16f1e0145adb5227f6af732781ce
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-09-25 23:18:35 +0100

    added a div with the className of app__header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { MenuItem, FormControl, Select } from '@material-ui/core'
 const App = () => {
   return (
     <div className="app">
-      <h1>COVID_19 TRACKER</h1>
-      <FormControl className="app__dropdown" >
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option one</MenuItem>
-          <MenuItem value="worldwide">Option two</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="app__header">
+        <h1>COVID_19 TRACKER</h1>
+        <FormControl className="app__dropdown" >
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option one</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   )
 }
