a6d238ba106e2dde940856df3132b735734ff8de
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-09-27 00:12:11 +0100

    Fetch countries API using useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MenuItem, FormControl, Select } from '@material-ui/core'
 import "./App.css"
 
 
 const App = () => {
+  const [countries, setCountries] = useState(['USA', "UK", "NIGERIA"])
+  const [selected, setSelected] = useState('NG')
+
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.coutry,
+            value: country.countryInfo.iso2
+          }))
+          setCountries(countries)
+        })
+    }
+    getCountriesData()
+  }, [])
+
   return (
     <div className="app">
-      <div className="app__header">
+      <div className="app__header container">
         <h1>COVID_19 TRACKER</h1>
         <FormControl className="app__dropdown" >
-          <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Option one</MenuItem>
-            <MenuItem value="worldwide">Option two</MenuItem>
+          <Select variant="outlined" value={selected} onChange={(e) => setSelected(e.target.value)}>
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+
+            ))}
+            {/* <MenuItem value="worldwide">Option one</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem> */}
           </Select>
         </FormControl>
       </div>
