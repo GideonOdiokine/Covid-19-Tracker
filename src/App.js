67daea54f9ede8e5513b1664a67cbd4df8eec95a
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-03 08:07:21 +0100

    Fetching data for worldwide data recorded and single country record

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core'
-import "./App.css"
-import InfoBoxs from './components/InfoBoxs'
-import Map from './components/Map'
-
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import "./App.css";
+import InfoBoxs from "./components/InfoBoxs";
+import Map from "./components/Map";
 
 const App = () => {
-  const [countries, setCountries] = useState(['USA', "UK", "NIGERIA"])
-  const [selected, setSelected] = useState('WorldWide')
+  const [countries, setCountries] = useState(["USA", "UK", "NIGERIA"]);
+  const [selected, setSelected] = useState("WorldWide");
 
-  const onCountryChange = (event) => {
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    setSelected(countryCode)
+    setSelected(countryCode);
 
-    const url = countryCode === 'WorldWide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
-  }
+    const url =
+      countryCode === "WorldWide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  };
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -23,26 +35,29 @@ const App = () => {
         .then((data) => {
           const countries = data.map((country) => ({
             name: country.country,
-            value: country.countryInfo.iso2
-          }))
-          setCountries(countries)
-        })
-    }
-    getCountriesData()
-  }, [])
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
 
   return (
     <div className="app">
       <div className="app__left">
         <div className="app__header ">
           <h1>COVID_19 TRACKER</h1>
-          <FormControl className="app__dropdown" >
-            <Select variant="outlined" value={selected} onChange={onCountryChange}>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={selected}
+              onChange={onCountryChange}
+            >
               <MenuItem value="WorldWide">WorldWide</MenuItem>
-              {countries.map(country => (
+              {countries.map((country) => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
               ))}
-
             </Select>
           </FormControl>
         </div>
@@ -55,13 +70,13 @@ const App = () => {
         <Map />
       </div>
       <Card className="app__right">
-        <CardContent >
+        <CardContent>
           <h3>Live case</h3>
           <h2>Worldwide cases</h2>
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
 export default App;
