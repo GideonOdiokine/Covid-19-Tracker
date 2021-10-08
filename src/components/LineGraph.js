9c9bb092707c92b6384fc1b2f6621cdb51e05b18
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-09 00:26:21 +0100

    formatted code using prettier extension

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { Line } from 'react-chartjs-2'
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 
 const LineGraph = () => {
     const [data, setData] = useState({});
 
     useEffect(() => {
-        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-    })
-    return (
-        <div>
-            <Line data />
-        </div>
-    )
-}
+        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120");
+        .then
+});
+return (
+    <div>
+        <Line data />
+    </div>
+);
+};
 
-export default LineGraph
+export default LineGraph;
