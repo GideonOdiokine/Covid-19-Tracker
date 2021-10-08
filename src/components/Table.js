7a8533899e9fd6e8d23d4e18c6e914308f2e8724
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-08 23:27:16 +0100

    destructured from countries the country and cases

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { createStructuredSelector } from 'reselect'
 
 export const Table = ({ countries }) => {
     return (
-        <div>
-
+        <div className="table">
+            {countries.map(({ country, cases }) => (
+                <tr>
+                    <td>{country}</td>
+                    <td>{cases}</td>
+                </tr>
+            ))}
         </div>
     )
 }
