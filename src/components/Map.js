684c52321cc062f41e0c7dd640dd3f50bfb92154
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-09 02:56:35 +0100

    added TileLayer

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,10 @@ const Map = () => {
     return (
         <div className="map" >
             <LeafletMap>
-                
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
             </LeafletMap>
         </div>
     )
