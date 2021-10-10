7bd63392938b98173f44d7767bf018adc1189feb
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-10 14:40:41 +0100

    added Marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import { Map as LeafletMap, TileLayer, Marker } from "react-leaflet";
+import L from 'leaflet'
 
 
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-icon-2x.png'),
+    iconUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-icon.png'),
+    shadowUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-shadow.png')
+});
 
 const Map = ({ center, zoom }) => {
     return (
@@ -11,6 +17,8 @@ const Map = ({ center, zoom }) => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+                <Marker position={center}>
+                </Marker>
             </LeafletMap>
         </div>
     )
