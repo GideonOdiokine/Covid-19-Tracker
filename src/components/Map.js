9d0ffdf3240ea58f5ee4ed60d086c9ee1a09c4f7
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-11 00:24:02 +0100

    added countries as props

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Map as LeafletMap, TileLayer, Marker } from "react-leaflet";
-import L from 'leaflet'
+import { Icon } from 'leaflet'
 
 
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-icon-2x.png'),
-    iconUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-icon.png'),
-    shadowUrl: require('https://rawcdn.githack.com/colbyfayock/egghead-code-examples/master/add-marker-popup-to-map-react-leaflet/src/images/marker-shadow.png')
+const icon = new Icon({
+    iconUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/390px-Map_marker.svg.png",
+    iconSize: [25, 25],
 });
 
-const Map = ({ center, zoom }) => {
+const Map = ({ center, zoom, countries }) => {
     return (
         <div className="map" >
             <LeafletMap center={center} zoom={zoom}>
@@ -17,7 +16,7 @@ const Map = ({ center, zoom }) => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={center}>
+                <Marker position={center} icon={icon}>
                 </Marker>
             </LeafletMap>
         </div>
