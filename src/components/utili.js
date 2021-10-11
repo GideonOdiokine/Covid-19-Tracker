ef38e034f39dd38033324ffd1c4bf33a90431ce2
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-10-11 01:02:29 +0100

    created showCiircleOnMap

--- src/components/utili.js
+++ src/components/utili.js
@@ -5,21 +5,21 @@ import { Circle, Popup } from 'react-leaflet';
 const casesTypeColors = {
     cases: {
         hex: "#CC1034",
-        rgb: "rgb(204, 16, 52)",
-        half_op: "rgba(204, 16, 52, 0.5)",
-        multiplier: 800,
+        // rgb: "rgb(204, 16, 52)",
+        // half_op: "rgba(204, 16, 52, 0.5)",
+        multiplier: 400,
     },
     recovered: {
         hex: "#7dd71d",
-        rgb: "rgb(125, 215, 29)",
-        half_op: "rgba(125, 215, 29, 0.5)",
-        multiplier: 1200,
+        // rgb: "rgb(125, 215, 29)",
+        // half_op: "rgba(125, 215, 29, 0.5)",
+        multiplier: 300,
     },
     deaths: {
         hex: "#fb4443",
-        rgb: "rgb(251, 68, 67)",
-        half_op: "rgba(251, 68, 67, 0.5)",
-        multiplier: 2000,
+        // rgb: "rgb(251, 68, 67)",
+        // half_op: "rgba(251, 68, 67, 0.5)",
+        multiplier: 500,
     },
 };
 
@@ -30,14 +30,19 @@ export const sortData = (data) => {
 
 }
 
-export const showDataOnMap = (data, casesType = "cases") => {
-    data.map(country => {
+export const showDataOnMap = (data, casesType = "cases") =>
+    data.map((country) => (
         <Circle
             center={[country.countryInfo.lat, country.countryInfo.long]}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
             fillOpacity={0.4}
-
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }
         >
-
+            <Popup>
+                <h1>I'm a Popup</h1>
+            </Popup>
         </Circle>
-    })
-}
+    ))
